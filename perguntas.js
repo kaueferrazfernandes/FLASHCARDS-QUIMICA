e1975ddcf9c0f9d4012bd7d55c8c2d58357d59e1
kaueferrazfernandes <133122827+kaueferrazfernandes@users.noreply.github.com>
2024-10-04 08:36:34 -0300

    Add files via upload
    PÁGINA FINALIZADA.

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Química orgânica',
+    'O que são hidrocarbonetos?',
+    'São compostos orgânicos formados apenas por átomos de carbono e hidrogênio'
+)
+
+criaCartao(
+    'Química orgânica',
+    'Qual a diferença entre uma cadeia carbônica saturada e uma insaturada?',
+    'Uma cadeia carbônica saturada contém apenas ligações simples entre os átomos de carbono, enquanto uma cadeia insaturada possui pelo menos uma ligação dupla ou tripla entre os átomos de carbono.'
+)
+
+criaCartao(
+    'Química orgânica',
+    'Qual é a diferença entre um alcano, alceno e alcino?',
+    'Alcanos têm apenas ligações simples entre carbonos, alcenos têm uma ligação dupla, e alcinos têm uma ligação tripla.'
+)
+
+criaCartao(
+    'Eletroquímica',
+    'O que é uma pilha eletroquímica e qual é sua função?',
+    'Uma pilha eletroquímica é um dispositivo que converte energia química em energia elétrica, através de reações de oxirredução.'
+)
+
+criaCartao(
+    'Eletroquímica',
+    'O que é uma reação de redução em uma célula eletroquímica?',
+    'Na redução, um átomo ou íon ganha elétrons. Na pilha, a redução ocorre no cátodo.'
+)
+
+criaCartao(
+    'Eletroquímica',
+    'Qual a diferença entre cátodo e ânodo em uma pilha?',
+    'O cátodo é o eletrodo onde ocorre a redução (ganho de elétrons), enquanto o ânodo é o eletrodo onde ocorre a oxidação (perda de elétrons).'
+)
+
+criaCartao(
+    'Termoquímica',
+    'O que é calor de reação?',
+    'O calor de reação é a quantidade de calor liberada ou absorvida durante uma reação química.'
+)
+
+criaCartao(
+    'Termoquímica',
+    'Qual a diferença entre uma reação endotérmica e uma reação exotérmica?',
+    'Reações endotérmicas absorvem calor do ambiente, enquanto reações exotérmicas liberam calor para o ambiente.'
+)
+
+criaCartao(
+    'Termoquímica',
+    'Como a variação de entalpia (ΔH) pode indicar se uma reação libera ou absorve energia?',
+    'Se a variação de entalpia (ΔH) for negativa, a reação é exotérmica (libera energia). Se for positiva, a reação é endotérmica (absorve energia).'
+)
